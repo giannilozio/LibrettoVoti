fb39d20098e6c542da62781442ef3e0c6960d24e
Lucio <Lucio@PC_Lucio>
2019-03-12 15:52:42 +0100

    Metodi standard aggiungti

--- src/it/polito/tdp/libreoo/Voto.java
+++ src/it/polito/tdp/libreoo/Voto.java
@@ -9,4 +9,26 @@ public class Voto {
 	private LocalDate data;
 	
 	
+	public int getVoto() {
+		return voto;
+	}
+	public void setVoto(int voto) {
+		this.voto = voto;
+	}
+	public String getCorso() {
+		return corso;
+	}
+	public void setCorso(String corso) {
+		this.corso = corso;
+	}
+	public LocalDate getData() {
+		return data;
+	}
+	public void setData(LocalDate data) {
+		this.data = data;
+	}
+	
+	
+	
+	
 }
